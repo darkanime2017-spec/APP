725d3bdd2396277421c9ec573791ee24d4d601b6
darkanime2017-spec <dark.anime.2017@gmail.com>
2025-12-13 15:02:36 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // You can change the port if needed
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+    define: {
+      'process.env': env
+    }
   }
-})
+});
